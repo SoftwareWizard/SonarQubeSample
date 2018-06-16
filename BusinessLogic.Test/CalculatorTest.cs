bdeed73354870a74ebbb44658210c009a9cae52d
Simon Bauer <ext.simon.bauer@it-designers.de>
2018-06-16 11:08:46 +0200

    Test implemented

---
+++ BusinessLogic.Test/CalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_Works()
+        {
+            // arrange
+            const int TestValueA = 1;
+            const int TestValueB = 1;
+
+            var testee = new Calculator();
+
+            // act
+            var result = testee.Add(TestValueA, TestValueB);
+
+            // assert
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ BusinessLogic.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusinessLogic.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusinessLogic.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44c1aea6-dc16-42df-b209-c08ab884b210")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
