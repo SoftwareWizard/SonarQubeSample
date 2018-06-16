0ca1ac759493c779a48f6814642f120b1576829d
Simon Bauer <ext.simon.bauer@it-designers.de>
2018-06-16 12:10:26 +0200

    used other scanner

--- test.bat
+++ test.bat
@@ -1,26 +1,25 @@
-@echo off
-
 SET TEST_FOLDER=BusinessLogic.Test\bin\Debug
 SET TEST_FILE=BusinessLogic.Test.dll
 
-SET SONAR_KEY=SimonsUniqueProjectKey
+SET SONAR_KEY=SimonsUniqueProject
 SET SONAR_PROJECT_NAME=SonarQubeSample
 SET SONAR_PROJECT_VERSION=1.0
 SET SONAR_URL=http://localhost:9000
-SET SONAR_LOGIN_TOKEN=d0d7ed429d24356f8c6321ff0d0b359b2b430d57
+SET SONAR_LOGIN_TOKEN=0c89b8abcfe390a5e172f976fa9a0f63a4241dc1
 SET SONAR_FOLDER=.sonarqube\results
 SET COVERAGE_FOLDER=covarageReport
-SET ANALYSER_FOLDER=analysisReport
 
 @echo --------------- Start SonarQube Tracking ---------------
-SonarScanner.MSBuild.exe begin^
- /k:"%SONAR_KEY%"^
- /n:"%SONAR_PROJECT_NAME%"
- /d:sonar.projectVersion="%SONAR_PROJECT_VERSION%"
- /d:sonar.host.url="%SONAR_URL%"^
- /d:sonar.login="%SONAR_LOGIN_TOKEN%"^
- /d:sonar.cs.dotcover.reportsPaths="%SONAR_FOLDER%\%COVERAGE_FOLDER%\dotCover.html"^
- /d:sonar.cs.analyzer.projectOutPaths="%SONAR_FOLDER%\%ANALYSER_FOLDER%"
+SonarQube.Scanner.MSBuild.exe begin /k:"%SONAR_KEY%" /d:sonar.host.url="%SONAR_URL%" /d:sonar.login="%SONAR_LOGIN_TOKEN%"
+
+
+REM SonarScanner.MSBuild.exe begin^
+REM  /k:"%SONAR_KEY%"^
+REM  /name:"%SONAR_PROJECT_NAME%"^
+REM  /version:"%SONAR_PROJECT_VERSION%"^
+REM  /d:sonar.host.url="%SONAR_URL%"^
+REM  /d:sonar.login="%SONAR_LOGIN_TOKEN%"^
+REM  /d:sonar.cs.dotcover.reportsPaths="%SONAR_FOLDER%\%COVERAGE_FOLDER%\dotCover.html"^
 
 @echo --------------- Start MSBuild ---------------
 MsBuild.exe /t:Rebuild
@@ -34,4 +33,5 @@ MsBuild.exe /t:Rebuild
  "/TargetArguments=%TEST_FOLDER%\%TEST_FILE%"
  
 @echo --------------- Generate Report ---------------
-SonarScanner.MSBuild.exe end /d:sonar.login="%SONAR_LOGIN_TOKEN%"
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="%SONAR_LOGIN_TOKEN%"
+REM SonarScanner.MSBuild.exe end /k:"%SONAR_KEY%" /d:sonar.login="%SONAR_LOGIN_TOKEN%"
